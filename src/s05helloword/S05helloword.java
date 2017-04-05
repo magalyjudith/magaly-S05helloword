d4f92df7d80208c93e9f2ef5fe8579bc08354665
user <user@user-PC>
2017-04-05 11:37:39 -0500

    last commit

---
+++ src/s05helloword/S05helloword.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s05helloword;
+
+/**
+ *
+ * @author user
+ */
+public class S05helloword {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World:");
+        System.out.println("Judith Ochoa");
+    }
+    
+}
